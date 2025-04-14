fa3224902148835ddabcd3ac30366565f59d8196
larakh51 <larakh51@gmail.com>
2025-04-14 15:27:49 +0300

    Add files via upload

---
+++ js_hw_1.js
@@ -0,0 +1,128 @@
+//////לרה חור-211565445-502
+
+/////////////////->Bool<-///////////////////////
+/////////////////-Task -1-///////////////////////
+
+// if("0"){
+//     alert('hello');
+// }
+/////////////////-Task -2-///////////////////////
+
+// let correct = "emcascript";
+// let guess =prompt("What's the official name of JavaScript?")
+// if(guess.toLocaleLowerCase()==correct){
+//     alert("Correct!");
+// }
+// else
+//     alert(`The official name is ${correct}`);
+/////////////////-Task -3-///////////////////////
+
+// let score = 75;
+// let grade = score>=90?"A":
+//     score>=80?"B":
+//     score>=70?"C":
+//     score>=60?"D":
+//     "F";
+//     alert(`your final grade is ${grade}`)
+/////////////////-Task -4-///////////////////////
+
+// let guess = prompt("Enter a number:")
+// if(guess>0)
+//     alert("1")
+// else if(guess<0)
+//     alert("-1")
+// else
+//     alert("0")
+/////////////////-Task -5-///////////////////////
+
+// let result;
+// result=(a+b<4)?"Not Enough":"A lot";
+
+/////////////////-Task -6-///////////////////////
+
+// let login ="Director";
+// let message = login=="Empolyee"?"HelloE":login=="Director"?"HelloD":login=''?"No login":'';
+// alert(message)
+
+/////////////////-Task -7-///////////////////////
+// console.log(fasle||'sdf');// True
+// console.log(fasle||'');//false
+// console.log(true||'');//true
+// console.log(true&&'');//false
+// console.log(undefined||null);//false
+// console.log(undefined||0||null);//false
+// console.log(undefined&&0&&null)//false
+// console.log(1||0);//true
+// console.log(null||1);//true
+// console.log(null||1||0);//true
+
+/////////////////////////////////////////////////
+/////////////////->Loops<-///////////////////////
+/////////////////-Task -1-///////////////////////
+// let i =0;
+// let sum=i;
+// while(i<101){
+//     sum+=i++;
+// }
+// alert(sum);
+/////////////////-Task -2-///////////////////////
+
+// let guess;
+// do{
+//     guess=Number(prompt("Enter a positive number"));
+// }while(isNaN(guess)||guess<=0);
+/////////////////-Task -3-///////////////////////
+
+// let num =7;
+// for(let i = 1;i<=10;i++){
+//     console.log(`${num} X ${i}=${num*i}`);
+// }
+/////////////////-Task -5-///////////////////////
+
+// for(let i =1;i<31;i++){
+//     if(i%3==0)
+//         continue;
+//     console.log(i);
+// }
+/////////////////-Task -6-///////////////////////
+/////////////////-Task -7-///////////////////////
+// let i =0;
+// while (++i<5) console.log(i);
+//1,2,3,4
+//---------------------------------------------//
+// let i =0;
+// while (i++<5) console.log(i);
+//0,1,2,3,4
+/////////////////-Task -8-///////////////////////
+// for(let i =0;i<5;i++) console.log(i)
+//0,1,2,3,4
+//---------------------------------------------//
+// for(let i=0;i<5;++i)console.log(i);
+//0,1,2,3,4
+/////////////////-Task -8-///////////////////////
+
+// let i =3;
+// while(i){
+//     alert(i--);
+// }
+// for(let i =2;i<=10;i++){
+//     if(i%2==0)
+//         continue;
+//     console.log(i);
+// }
+/////////////////-Task -9-///////////////////////
+
+// let i=1;
+// while(i<10){
+//     i++;
+//     if(i%2==0)
+//         continue;
+//     console.log(i);
+
+// }
+/////////////////-Task -10-///////////////////////
+
+// let i = 0;
+// while(i<3){
+//     alert(`number ${i}!`)
+// }
